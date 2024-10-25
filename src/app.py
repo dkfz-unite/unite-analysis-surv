7495e1751fbaaa2ae5836603879590d01e28a225
h600s <harold.matthews@dkfz-heidelberg.de>
2024-10-25 12:36:47 +0200

    major refactoring to support analysis stratified by group and produce test statistic comparing curves

--- src/app.py
+++ src/app.py
@@ -1,9 +1,11 @@
 import argparse
 import pandas as pd
+import numpy as np
 from typing import Tuple
 import os
 import sys
 from sksurv.nonparametric import kaplan_meier_estimator
+from sksurv.compare import compare_survival
 
 def load_data(data_path : str) -> pd.DataFrame:
     """Reads a tsv file into a pandas.DataFrame
@@ -16,49 +18,84 @@ def load_data(data_path : str) -> pd.DataFrame:
     data = pd.read_csv(data_path,sep="\t")
     return data
 
-def get_survival_days_from_dates(data : pd.DataFrame) -> Tuple[pd.Series, pd.Series]:
+def get_survival_days_from_dates(data : pd.DataFrame,census_date_column : str) -> Tuple[pd.Series, pd.Series]:
     """Get the number of days survival given the diagnosis date and the last follow up date.
 
-    :param data: A pandas dataframe with the columns "diagnosis_date" and "vital_status_change_date"
+    :param data: A pandas dataframe with the columns "diagnosis_date" and "vital_status_change_date" or "progression_status_change_date"
     :type data: pd.DataFrame
+    :param census_date_column: the name of the column containing the last follow up date this can be either "vital_status_change_date" or "progression_status_change_date"
+    :type census_date_column: str
     :return: A tuple with the survival days and a boolean series indicating if the calculation returned a not null value
     :rtype: Tuple[pd.Series, pd.Series]
     """
+    # check the value of the census_date_column
+    if census_date_column not in ["vital_status_change_date","progression_status_change_date"]:
+        raise ValueError(f"Column {census_date_column} not in ['vital_status_change_date','progression_status_change_date']")
+    if census_date_column not in data.columns:
+        raise ValueError(f"Column {census_date_column} not in data")
     
     diag_date = pd.to_datetime(data["diagnosis_date"])
-    census_date = pd.to_datetime(data["vital_status_change_date"])
+    census_date = pd.to_datetime(data[census_date_column])
     survival_days = (census_date - diag_date).dt.days
     success=survival_days.isna()==False
     return survival_days, success
 
-def get_survival_days_from_days(data : pd.DataFrame) -> Tuple[pd.Series, pd.Series]:
+def get_survival_days_from_days(data : pd.DataFrame, census_day_column : str) -> Tuple[pd.Series, pd.Series]:
     """Get the number of days survival given the number of days survival (specified exactly in the data frame
 
-    :param data: A pandas dataframe with the column "vital_status_change_day"
+    :param data: A pandas dataframe with the column "vital_status_change_day" or "progression_status_change_day"
     :type data: pd.DataFrame
+    :type census_day_column: the name of the column containing the last follow up day this can be either "vital_status_change_day" or "progression_status_change_day"
     :return: A tuple with the survival days and a boolean series indicating if the number of days survival is not null
     :rtype: Tuple[pd.Series, pd.Series]
     """
+    # check the value of the census_day_column
+    if census_day_column not in ["vital_status_change_day","progression_status_change_day"]:
+        raise ValueError(f"Column {census_day_column} not in ['vital_status_change_day','progression_status_change_day']")
+    if census_day_column not in data.columns:
+        raise ValueError(f"Column {census_day_column} not in data")
+    
     survival_days=data["vital_status_change_day"]
     survival_days.name="survival_days"
     survival_days=survival_days.astype(float)
     success=survival_days.isna()==False
     return survival_days, success
 
-def get_survival_days(data : pd.DataFrame) -> pd.Series:
+def get_survival_columns(type : str) -> Tuple[str,str]:
+    """Get the names of the columns containing the survival dates and the last follow up dates
+
+    :param type: the type of survival days to get. This can be either "progress" for progression free survival or "vital" for overall survival
+    :type type: str
+    :return: A tuple with the names of the columns containing the survival dates and the last follow up dates
+    :rtype: Tuple[str,str]
+    """
+    if type not in ["progress","vital"]:
+        raise ValueError(f"Type {type} not in ['progress','vital']")
+    if type=="progress":
+        return "progression_status_change_date","progression_status_change_day"
+    else:
+        return "vital_status_change_date","vital_status_change_day"
+
+
+
+def get_survival_days(data : pd.DataFrame,type : str) -> pd.Series:
     """Get the number of days survival from the data frame.
-    :param data: 
+    :param data: the input data frame
     :type data: pd.DataFrame
+    :param type: the type of survival days to get. This can be either "progress" for progression free survival or "vital" for overall survival
     :return: pd.DataFrame
     """
+    
+    # get the names of the columns containing the survival dates and the last follow up dates depending on the type
+    date_col,day_col=get_survival_columns(type)
     # initialise data frame
     survival_days = pd.Series(index=range(len(data)),name="survival_days",dtype=float)
     # try to get survival days from the dates
-    s1,success = get_survival_days_from_dates(data)
+    s1,success = get_survival_days_from_dates(data,date_col)
     
-    survival_days[success]=s1[success]
+    survival_days.iloc[np.array(success)]=s1.iloc[np.array(success)]
     # if not possible, get survival days from the days
-    survival_days[~success]= get_survival_days_from_days(data)[0][~success]
+    survival_days.iloc[np.array(~success)]= get_survival_days_from_days(data,day_col)[0].iloc[np.array(~success)]
     return survival_days
     
 def estimate_survival_function(survival : pd.Series, exit_status : pd.Series) -> pd.DataFrame:
@@ -81,43 +118,141 @@ def estimate_survival_function(survival : pd.Series, exit_status : pd.Series) ->
     })
     return survival_df
 
-def get_censored_df(survival_days : pd.Series, exit_status : pd.Series, data : pd.DataFrame) -> pd.DataFrame:
+def get_censored_df(survival_days : pd.Series, exit_status : pd.Series, ids : pd.Series) -> pd.DataFrame:
     """Return a data frame containing the survival days of all censored patients (those who were still alive at last follow up)
     
     :param survival_days: the days survival of all patients
     :type survival_days: pd.Series
     :param exit_status: the exit status of all patients (True if dead, False if alive)
     :type exit_status: pd.Series
-    :param data: the input data frame
-    :type data: pd.DataFrame
+    :param ids: the ids of the patients
+    :type ids: pd.DataFrame
     :return: a data frame containing the survival days of all censored patients. It will contain two columns 'sample_id' 'days_at_censoring'
     :rtype: pd.DataFrame
     """
     
     censored=exit_status==False
-    sample_id=data["sample_id"][censored]
-    days_at_censoring=survival_days[censored]
+    sample_id=ids[censored]
+    days_at_censoring=survival_days.iloc[np.array(censored)]
     censored_df=pd.DataFrame({"sample_id":sample_id,"days_at_censoring":days_at_censoring})
     return censored_df
 
+def get_exit_status(data : pd.DataFrame,type : str) -> pd.Series:
+    """Get the exit status of all patients. If type is "vital" this should be true if they are dead and false if they are alive. If type is "progress" this should be true if they have progressed and false if they have not.
+
+    :param data: the input data frame
+    :type data: pd.DataFrame
+    :param type: the type of survival days. This can be either "progress" for progression free survival or "vital" for overall survival
+    :return: a boolean series indicating if the patient died
+    :rtype: pd.Series
+    """
+    # check type value
+    if type not in ["progress","vital"]:
+        raise ValueError(f"Type {type} not in ['progress','vital']")
+    
+    if type == "vital":
+        return data["vital_status"]==False
+    
+    if type == "progress":
+        return data["progression_status"]==True
 
 
-def main(root_path : str):
-    """Main function to estimate the survival function from the input data and write the result to a file result.tsv. Also writes censored.tsv which contains the survival days of all right censored patients.
+def get_labels(data : pd.DataFrame) -> pd.Series:
+    """Get the labels of the groups in the data frame
+
+    :param data: the input data frame
+    :type data: pd.DataFrame
+    :return: a series with the labels
+    :rtype: pd.Series
+    """
+    if "label" not in data.columns:
+        return pd.Series(np.zeros(len(data),dtype=int),dtype=str)
+    return data["label"]
+
+def get_subsets(data : pd.DataFrame,label : pd.Series) -> list:
+    """Get the subsets of the data frame for each label
+    
+    :param data: the input data frame
+    :type data: pd.DataFrame
+    """
+    return [data.iloc[np.array(label==l),:] for l in label.unique()],label.unique()
+
+def get_survival_function_and_censored_dfs(survival_days : pd.Series,exit_status : pd.Series,ids : pd.Series) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    """Get the survival function and censored data frame from the survival days and exit status
+
+    :param survival_days: the number of days survival
+    :type survival_days: pd.Series
+    :param exit_status: the exit status of the patients
+    :type exit_status: pd.Series
+    :param ids: the ids of the patients
+    :type data: pd.DataFrame
+    :return: a tuple with the survival function and the censored data frame
+    :rtype: Tuple[pd.DataFrame, pd.DataFrame
+    
+    """
+    output = estimate_survival_function(survival_days, exit_status)
+    output_censored = get_censored_df(survival_days, exit_status, ids)
+    return output, output_censored
+
+
+def logrank_test(survival_days : pd.Series,exit_status : pd.Series,label : pd.Series) -> pd.DataFrame:
+    """Perform the logrank test comparing the survival curves of two or more groups
+
+    :param survival_days: the number of days survival
+    :type survival_days: pd.Series
+    :param exit_status: the exit status of the patients
+    :type exit_status: pd.Series
+    :param label: the labels of the groups
+    :type label: pd.Series
+    :return: a data frame with the chi2 and p values of the logrank test
+    :rtype: pd.DataFrame
+    """
+    # crate a structured array with the survival days and exit status
+    survival_data = np.zeros(len(survival_days), dtype=[('status', bool), ('time', float)])
+    survival_data['status'] = exit_status
+    survival_data['time'] = survival_days
+    chi2, p = compare_survival(survival_data, label)   
+    return pd.DataFrame({"chi2":chi2,"p":p},index=["logrank_test"]) 
+
+
+def main(root_path : str,type : str):
+    """Main function to estimate the survival functions from the input data and write the result to a file result.tsv. Also writes censored.tsv which contains the survival days of all right censored patients.
+    Does this separately for each group label in the input data (in the "label") column.
     
     :param root_path: the path in which the input.tsv file is located and where the output files will be written
+    :param type: the type of survival days to get. This can be either "progress" for progression free survival or "vital" for overall survival
     :type root_path: str
     """
-    data = load_data(os.path.join(root_path, "input.tsv"))  
-    survival_days = get_survival_days(data)
-    exit_status=data["vital_status"]==False #True if dead, False if alive
-    output = estimate_survival_function(survival_days, exit_status)
-    output_censored = get_censored_df(survival_days, exit_status, data)
-    # write output to files
-    output.to_csv(os.path.join(root_path,"result.tsv"), index=False,sep="\t")
-    output_censored.to_csv(os.path.join(root_path,"censored.tsv"), index=False,sep="\t")
-    print("Done")
+    # load the data
+    data = load_data(os.path.join(root_path,"input.tsv"))
+    # get the labels of the groups
+    label = get_labels(data)
+    # get the data subsetted by label
+    subsets,unique_labels = get_subsets(data,label)
+    # get the survival days and exit status (event binary indicator) for each group
+    survival_days = [get_survival_days(subset,type) for subset in subsets]
+    exit_status = [get_exit_status(subset,type) for subset in subsets]
     
+    # print the length of the survival days and exit status
+    print([len(s) for s in survival_days])
+    print([len(s) for s in exit_status])
+    print(unique_labels)
+    
+    # get the survival function and censored data data frame for each group
+    survival_functions, censored_dfs = zip(*[get_survival_function_and_censored_dfs(s,e,d.sample_id) for s,e,d in zip(survival_days,exit_status,subsets)])    
+    
+    # write the survival functions each to a separate file
+    for l,s,c in zip(unique_labels,survival_functions,censored_dfs):
+        s.to_csv(os.path.join(root_path,f"result{l}.tsv"), sep="\t", index=False)
+        c.to_csv(os.path.join(root_path,f"censored{l}.tsv"), sep="\t", index=False)
+    
+    # perform the logrank test if there is more than one group
+    if len(unique_labels) > 1:
+        logrank = logrank_test(pd.concat(survival_days),pd.concat(exit_status),label)
+        logrank.to_csv(os.path.join(root_path,"logrank_test.tsv"), sep="\t", index=False)
+    
+
 if __name__ == "__main__":
     root_path = sys.argv[1]
-    main(root_path)
+    type = sys.argv[2]
+    main(root_path,type)
--- test/test_app.py
+++ test/test_app.py
@@ -4,43 +4,77 @@ import sys
 sys.path.append("./src")
 from app import estimate_survival_function
 import pytest
+from unittest.mock import patch
 import pandas as pd
-from app import get_survival_days_from_dates, get_survival_days_from_days, get_survival_days, load_data, get_censored_df
+from app import get_survival_days_from_dates, get_survival_days_from_days, get_survival_days, load_data, get_censored_df, get_survival_columns, get_exit_status, get_labels,get_subsets,  logrank_test, main
+import numpy as np
+import os
 
 @pytest.fixture
 def mock_survival_data():
     survival = pd.Series([5, 10, 15, 20, 25])
     exit_status = pd.Series([True, False, True, False, True])
-    return survival, exit_status
+    label = pd.Series(["A", "B", "A", "B", "A"])
+    return survival, exit_status,label
 
 @pytest.fixture
 def mock_data_df():
     data = pd.DataFrame({
         "diagnosis_date": ["2020-01-01", "2020-02-01", "2020-03-01"],
         "vital_status_change_date": ["2020-01-10", None, "2020-03-15"],
-        "vital_status_change_day": [9, 20, 14]
+        "vital_status_change_day": [9, 20, 14],
+        "vital_status": [False, True, False],
+        "progression_status": [True, False, True],
+        "progression_status_change_date": ["2020-01-10", None, "2020-03-15"],
+        "progression_status_change_day": [9, 20, 14],
+        "label": ["A", "B", "A"],
+        "sample_id": ["1", "2", "3"]
     })
+    return data
+@pytest.fixture
+def mock_data_df_truncated(mock_data_df):
+    data = mock_data_df.copy()
+    data = data[[True, False, True]]
     return data
 
 
 def test_load_data():
-    data = load_data("test/data/input.tsv")
+     data = load_data("test/data/input.tsv")
     
-    # Check if the result is a DataFrame
-    assert isinstance(data, pd.DataFrame)
+     # Check if the result is a DataFrame
+     assert isinstance(data, pd.DataFrame)
     
-    # Check if the DataFrame is not empty
-    assert not data.empty
-    # Check if the DataFrame has the correct number of columns
-    assert len(data.columns) == 5
-    # Check if the DataFrame has the correct columns
-    expected_columns = ["sample_id","diagnosis_date","vital_status","vital_status_change_date","vital_status_change_day"]
-    assert list(data.columns) == expected_columns
+     # Check if the DataFrame is not empty
+     assert not data.empty
+     # Check if the DataFrame has the correct number of columns
+     assert len(data.columns) == 9
+     # Check if the DataFrame has the correct columns
+     expected_columns = ["sample_id","diagnosis_date","vital_status","vital_status_change_date","vital_status_change_day","progression_status","progression_status_change_date","progression_status_change_day","label"]
+     assert list(data.columns) == expected_columns
+
+def test_get_labels(mock_data_df):
+    """ test the labels column is returned or a series of 0s if the column is not present"""
+    assert get_labels(mock_data_df).equals(pd.Series(["A", "B", "A"]))
+    # remove the label column
+    mock_data_df.drop("label", axis=1, inplace=True)
+    assert get_labels(mock_data_df).equals(pd.Series(["0", "0", "0"]))
 
 
+def test_get_subsets(mock_data_df):
+    """ test the subsets of the data are returned """
+    labels = mock_data_df.label
+    subsets,unique_labels = get_subsets(mock_data_df,labels)
+    
+    # check out put is a list of dataframes
+    assert isinstance(subsets,list)
+    assert all(isinstance(subset,pd.DataFrame) for subset in subsets)
+    # check the subsets are as expected
+    assert subsets[0].equals(mock_data_df.iloc[np.array(mock_data_df.label=="A"),:])
+    assert subsets[1].equals(mock_data_df.iloc[np.array(mock_data_df.label=="B"),:])
 
-def test_get_survival_days_from_dates(mock_data_df):
-    survival_days, success = get_survival_days_from_dates(mock_data_df)
+@pytest.mark.parametrize("census_date_column", ["progression_status_change_date", "vital_status_change_date"])
+def test_get_survival_days_from_dates(mock_data_df,census_date_column):
+    survival_days, success = get_survival_days_from_dates(mock_data_df,census_date_column)
     
     # Check if the result is a Series
     assert isinstance(survival_days, pd.Series)
@@ -51,12 +85,21 @@ def test_get_survival_days_from_dates(mock_data_df):
     pd.testing.assert_series_equal(survival_days, expected_survival_days)
     
     # Check if the success Series has the correct values
-    expected_success = pd.Series([True, False, True])
+    expected_success = pd.Series([ True,False,True])
+    pd.testing.assert_series_equal(success, expected_success)
+    
+    # try on truncated data
+    trunc_data = mock_data_df.copy()
+    trunc_data = trunc_data.iloc[[False, True, True],:]
+    survival_days, success = get_survival_days_from_dates(trunc_data,census_date_column)
+    expected_survival_days = pd.Series([None, 14],index=[1,2])
+    pd.testing.assert_series_equal(survival_days, expected_survival_days)
+    expected_success = pd.Series([False, True],index=[1,2])
     pd.testing.assert_series_equal(success, expected_success)
 
-
-def test_get_survival_days_from_days(mock_data_df):
-    survival_days, success = get_survival_days_from_days(mock_data_df)
+@pytest.mark.parametrize("census_day_column", ["progression_status_change_day", "vital_status_change_day"])
+def test_get_survival_days_from_days(mock_data_df,census_day_column):
+    survival_days, success = get_survival_days_from_days(mock_data_df,census_day_column)
     
     # Check if the result is a Series
     assert isinstance(survival_days, pd.Series)
@@ -69,11 +112,19 @@ def test_get_survival_days_from_days(mock_data_df):
     # Check if the success Series has the correct values
     expected_success = pd.Series([True, True, True],name="survival_days")
     pd.testing.assert_series_equal(success, expected_success)
+    
+    # try on truncated data (i.e. after subsetting)
+    trunc_data = mock_data_df.copy()
+    trunc_data = trunc_data.iloc[[False, True, True],:]
+    survival_days, success = get_survival_days_from_days(trunc_data,census_day_column)
+    expected_survival_days = pd.Series([20, 14],index=[1,2],name="survival_days",dtype=float)
+    pd.testing.assert_series_equal(survival_days, expected_survival_days)
+    expected_success = pd.Series([True, True],index=[1,2],name="survival_days")
 
+@pytest.mark.parametrize("type", ["progress", "vital"])
+def test_get_survival_days(mock_data_df,type):
 
-def test_get_survival_days(mock_data_df):
-
-    survival_days = get_survival_days(mock_data_df)
+    survival_days = get_survival_days(mock_data_df,type)
     
     # Check if the result is a Series
     assert isinstance(survival_days, pd.Series)
@@ -81,12 +132,30 @@ def test_get_survival_days(mock_data_df):
     # Check if the Series has the correct values
     expected_survival_days = pd.Series([9, 20, 14],name="survival_days",dtype=float)
     pd.testing.assert_series_equal(survival_days, expected_survival_days)
+     
+    # try on truncated data (i.e. after subsetting)
+    trunc_data = mock_data_df.copy()
+    survival_days = get_survival_days(trunc_data,type)
+    expected_survival_days = pd.Series([20, 14],index=[1,2],name="survival_days",dtype=float)
 
-
+@pytest.mark.parametrize("type", ["progress", "vital"])
+def test_get_exit_status(mock_data_df,type):
+    exit_status = get_exit_status(mock_data_df,type)
+    
+    # Check if the result is a Series
+    assert isinstance(exit_status, pd.Series)
+    
+    # Check if the Series has the correct values
+    if type == "progress":
+        expected_exit_status = pd.Series([True, False, True],name="progression_status",dtype=bool)
+    if type == "vital":
+        expected_exit_status = pd.Series([True, False, True],name="vital_status",dtype=bool)
+    pd.testing.assert_series_equal(exit_status, expected_exit_status)
+    
 
 
 def test_estimate_survival_function(mock_survival_data):
-    survival, exit_status = mock_survival_data
+    survival, exit_status,_ = mock_survival_data
     result = estimate_survival_function(survival, exit_status)
     
     # Check if the result is a DataFrame
@@ -112,7 +181,7 @@ def mock_data_for_censored():
 
 def test_get_censored_df(mock_data_for_censored):
     survival_days, exit_status, data = mock_data_for_censored
-    result = get_censored_df(survival_days, exit_status, data)
+    result = get_censored_df(survival_days, exit_status, data.sample_id)
     
     # Check if the result is a DataFrame
     assert isinstance(result, pd.DataFrame)
@@ -127,6 +196,108 @@ def test_get_censored_df(mock_data_for_censored):
         "days_at_censoring": [10, 20]
     }, index=[1, 3])
     pd.testing.assert_frame_equal(result, expected_data)
+
+@pytest.mark.parametrize("type", ["progress", "vital"])
+def test_get_survival_columns(type):
+    result = get_survival_columns(type)
+    if type == "progress":
+        expected_columns = ("progression_status_change_date","progression_status_change_day")
+    if type == "vital":
+        expected_columns = ("vital_status_change_date","vital_status_change_day")
+    assert result == expected_columns   
+
+
+@patch("app.load_data")
+@patch("app.get_labels")
+@patch("app.get_subsets")
+@patch("app.get_survival_days")
+@patch("app.get_exit_status")
+@patch("app.get_survival_function_and_censored_dfs")
+@patch("app.logrank_test")
+@patch("app.pd.DataFrame.to_csv")
+def test_main(mock_to_csv, mock_logrank_test, mock_get_survival_function_and_censored_dfs, mock_get_exit_status, mock_get_survival_days, mock_get_subsets, mock_get_labels, mock_load_data, mock_data_df, tmp_path):
+    # Mock the load_data function to return the mock data
+    mock_load_data.return_value = mock_data_df
+    
+    # Mock the get_labels function to return the labels
+    mock_get_labels.return_value = mock_data_df.label
+    
+    # Mock the get_subsets function to return subsets of the data
+    mock_get_subsets.return_value = ([mock_data_df.iloc[:2], mock_data_df.iloc[2:]], ["A", "B"])
+    
+    # Mock the get_survival_days function to return survival days - 'side effect' is used to return different values for each call
+    mock_get_survival_days.side_effect = [pd.Series([9, 19]), pd.Series([14, 9])]
+    
+    # Mock the get_exit_status function to return exit status
+    mock_get_exit_status.side_effect = [pd.Series([False, True]), pd.Series([False, True])]
+    
+    # Mock the get_survival_function_and_censored_dfs function to return survival functions and censored data frames
+    mock_get_survival_function_and_censored_dfs.side_effect = [
+        (pd.DataFrame({"time": [9, 19], "survival_prob": [0.9, 0.8]}), pd.DataFrame({"sample_id": ["A"], "days_at_censoring": [9]})),
+        (pd.DataFrame({"time": [14, 9], "survival_prob": [0.7, 0.6]}), pd.DataFrame({"sample_id": ["C"], "days_at_censoring": [14]}))
+    ]
+    
+    # Mock the logrank_test function to return a DataFrame
+    mock_logrank_test.return_value = pd.DataFrame({"test_statistic": [1.23], "p_value": [0.45]})
+    
+    
+    # Call the main function
+    main(str(tmp_path), "vital")
+    
+    # Check if the load_data function was called
+    mock_load_data.assert_called_once_with(os.path.join(str(tmp_path), "input.tsv"))
+    
+    # Check if the get_labels function was called
+    mock_get_labels.assert_called_once_with(mock_data_df)
+    
+    # Check if the get_subsets function was called
+    mock_get_subsets.assert_called_once_with(mock_data_df, mock_data_df.label)
+    
+    # Check if the get_survival_days function was called twice
+    assert mock_get_survival_days.call_count == 2
+    
+    # Check if the get_exit_status function was called twice
+    assert mock_get_exit_status.call_count == 2
+    
+    # Check if the get_survival_function_and_censored_dfs function was called twice
+    assert mock_get_survival_function_and_censored_dfs.call_count == 2
+    
+    # Check if the to_csv function was called for each survival function and censored data frame
+    assert mock_to_csv.call_count == 5
+    
+    # Check if the logrank_test function was called
+    mock_logrank_test.assert_called_once()
+    
+    # Verify the filenames used in to_csv calls
+    expected_files = [
+        os.path.join(str(tmp_path), "resultA.tsv"),
+        os.path.join(str(tmp_path), "censoredA.tsv"),
+        os.path.join(str(tmp_path), "resultB.tsv"),
+        os.path.join(str(tmp_path), "censoredB.tsv"),
+        os.path.join(str(tmp_path), "logrank_test.tsv")
+    ]
+    actual_files = [call[0][0] for call in mock_to_csv.call_args_list]
+    assert actual_files == expected_files
+
+def test_logrank_test(mock_survival_data):
+    survival, exit_status,label = mock_survival_data
+    result = logrank_test(survival,exit_status,label)
+    
+    # Check if the result is a DataFrame
+    assert isinstance(result, pd.DataFrame)
+    
+    # Check if the DataFrame has the correct columns
+    expected_columns = ["chi2", "p"]
+    assert list(result.columns) == expected_columns
+    
+    # Check if the DataFrame has the correct number of rows
+    assert len(result) == 1
+    
+    # Check if the DataFrame has the correct values (example values, adjust as needed)
+    assert result["chi2"].iloc[0] == pytest.approx(1.163, rel=1e-2)
+    assert result["p"].iloc[0] == pytest.approx(0.280, rel=1e-2)
+
+
 
 # Run the tests
 if __name__ == "__main__":
