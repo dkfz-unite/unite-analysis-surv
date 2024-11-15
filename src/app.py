f15abd5e27f4278162de88a57386cf842f432ccf
h600s <harold.matthews@dkfz-heidelberg.de>
2024-11-15 09:56:57 +0100

    adapt for new input file format

--- src/app.py
+++ src/app.py
@@ -22,16 +22,16 @@ def load_data(data_path : str) -> pd.DataFrame:
 def get_survival_days_from_dates(data : pd.DataFrame) -> Tuple[pd.Series, pd.Series]:
     """Get the number of days survival given the diagnosis date and the last follow up date.
 
-    :param data: A pandas dataframe with the columns "diagnosis_date" and "vital_status_change_date" or "progression_status_change_date"
+    :param data: A pandas dataframe with the columns "enrolment_date" and "status_change_date"
     :type data: pd.DataFrame
     :return: A tuple with the survival days and a boolean series indicating if the calculation returned a not null value
     :rtype: Tuple[pd.Series, pd.Series]
     """
 
     
-    diag_date = pd.to_datetime(data["diagnosis_date"])
+    enrolment_date = pd.to_datetime(data["enrolment_date"])
     census_date = pd.to_datetime(data["status_change_date"])
-    survival_days = (census_date - diag_date).dt.days
+    survival_days = (census_date - enrolment_date).dt.days
     success=survival_days.isna()==False
     return survival_days, success
 
@@ -101,55 +101,42 @@ def get_censored_df(survival_days : pd.Series, exit_status : pd.Series, ids : pd
     :type exit_status: pd.Series
     :param ids: the ids of the patients
     :type ids: pd.DataFrame
-    :return: a data frame containing the survival days of all censored patients. It will contain two columns 'sample_id' 'days_at_censoring'
+    :return: a data frame containing the survival days of all censored patients. It will contain two columns 'donor_id' 'days_at_censoring'
     :rtype: pd.DataFrame
     """
     
     censored=exit_status==False
-    sample_id=ids[censored]
+    donor_id=ids[censored]
     days_at_censoring=survival_days.iloc[np.array(censored)]
-    censored_df=pd.DataFrame({"sample_id":sample_id,"days_at_censoring":days_at_censoring})
+    censored_df=pd.DataFrame({"donor_id":donor_id,"days_at_censoring":days_at_censoring})
     return censored_df
 
-def get_exit_status(data : pd.DataFrame,type : str) -> pd.Series:
-    """Get the exit status of all patients. If type is "survival" this should be true if they are dead and false if they are alive. If type is "progression" this should be true if they have progressed and false if they have not.
+def get_exit_status(data : pd.DataFrame) -> pd.Series:
+    """Get the exit status of all patients. 
+    :param data: the input data frame
+    :type data: pd.DataFrame
+    :rtype: pd.Series
+    """
+    return data["status"]
+def get_dataset_ids(data : pd.DataFrame) -> pd.Series:
+    """Get the dataset_ids of the groups in the data frame
 
     :param data: the input data frame
     :type data: pd.DataFrame
-    :param type: the type of survival days. This can be either "progression" for progression free survival or "survival" for overall survival
-    :return: a boolean series indicating if the patient died
+    :return: a series with the dataset_ids
     :rtype: pd.Series
     """
-    # check type value
-    if type not in ["progression","survival"]:
-        raise ValueError(f"Type {type} not in ['progression','survival']")
-    
-    if type == "survival":
-        return data["status"]==False
-    
-    if type == "progression":
-        return data["status"]==True
-
-
-def get_labels(data : pd.DataFrame) -> pd.Series:
-    """Get the labels of the groups in the data frame
-
-    :param data: the input data frame
-    :type data: pd.DataFrame
-    :return: a series with the labels
-    :rtype: pd.Series
-    """
-    if "label" not in data.columns:
+    if "dataset_id" not in data.columns:
         return pd.Series(np.zeros(len(data),dtype=int),dtype=str)
-    return data["label"]
+    return data["dataset_id"]
 
-def get_subsets(data : pd.DataFrame,label : pd.Series) -> list:
-    """Get the subsets of the data frame for each label
+def get_subsets(data : pd.DataFrame,dataset_id : pd.Series) -> list:
+    """Get the subsets of the data frame for each dataset_id
     
     :param data: the input data frame
     :type data: pd.DataFrame
     """
-    return [data.iloc[np.array(label==l),:] for l in label.unique()],label.unique()
+    return [data.iloc[np.array(dataset_id==l),:] for l in dataset_id.unique()],dataset_id.unique()
 
 def get_survival_function_and_censored_dfs(survival_days : pd.Series,exit_status : pd.Series,ids : pd.Series) -> Tuple[pd.DataFrame, pd.DataFrame]:
     """Get the survival function and censored data frame from the survival days and exit status
@@ -169,15 +156,15 @@ def get_survival_function_and_censored_dfs(survival_days : pd.Series,exit_status
     return output, output_censored
 
 
-def logrank_test(survival_days : pd.Series,exit_status : pd.Series,label : pd.Series) -> pd.DataFrame:
+def logrank_test(survival_days : pd.Series,exit_status : pd.Series,dataset_id : pd.Series) -> pd.DataFrame:
     """Perform the logrank test comparing the survival curves of two or more groups
 
     :param survival_days: the number of days survival
     :type survival_days: pd.Series
     :param exit_status: the exit status of the patients
     :type exit_status: pd.Series
-    :param label: the labels of the groups
-    :type label: pd.Series
+    :param dataset_id: the dataset_ids of the groups
+    :type dataset_id: pd.Series
     :return: a data frame with the chi2 and p values of the logrank test
     :rtype: pd.DataFrame
     """
@@ -185,69 +172,57 @@ def logrank_test(survival_days : pd.Series,exit_status : pd.Series,label : pd.Se
     survival_data = np.zeros(len(survival_days), dtype=[('status', bool), ('time', float)])
     survival_data['status'] = exit_status
     survival_data['time'] = survival_days
-    chi2, p = compare_survival(survival_data, label)   
+    chi2, p = compare_survival(survival_data, dataset_id)   
     return pd.DataFrame({"chi2":chi2,"p":p},index=["logrank_test"]) 
 
-def concatenate_dfs_add_label(dfs : list,label : str) -> pd.DataFrame:
-    """Concatenate a list of data frames and add a column with the label
+def concatenate_dfs_add_dataset_id(dfs : list,dataset_id : str) -> pd.DataFrame:
+    """Concatenate a list of data frames and add a column with the dataset_id
 
     :param dfs: a list of data frames
     :type dfs: list
-    :param label: the label to add to the data frames
-    :type label: str
-    :return: a data frame with the data frames concatenated and the label column added
+    :param dataset_id: the dataset_id to add to the data frames
+    :type dataset_id: str
+    :return: a data frame with the data frames concatenated and the dataset_id column added
     :rtype: pd.DataFrame
     """
     for i,df in enumerate(dfs):
-        df["label"]=label[i]
+        df["dataset_id"]=dataset_id[i]
     return pd.concat(dfs,ignore_index=True)
 
-
-def read_options_file(root_path : str) -> dict:
-    # read the options from the options file
-    with open(os.path.join(root_path,"options.json"), 'r') as f:
-        config = json.load(f)
-    
-    # Get the type of survival days to get from the JSON file
-    mode = config.get("mode")
-    assert mode in ["progression","survival"], "mode must be either 'progression' or 'survival'"
-    return {'mode':mode}
     
 def main(root_path : str):
     """Main function to estimate the survival functions from the input data and write the result to a file result.tsv. Also writes censored.tsv which contains the survival days of all right censored patients.
-    Does this separately for each group label in the input data (in the "label") column.
+    Does this separately for each group dataset_id in the input data (in the "dataset_id") column.
     
     :param type: the type of survival days to get. This can be either "progress" for progression free survival or "vital" for overall survival
     :type root_path: str
     """
     # load the data
     data = load_data(os.path.join(root_path,"input.tsv"))
-    opts = read_options_file(root_path)
-    mode = opts["mode"]
     
-    # get the labels of the groups
-    label = get_labels(data)
-    # get the data subsetted by label
-    subsets,unique_labels = get_subsets(data,label)
+    # get the dataset_ids of the groups
+    dataset_id = get_dataset_ids(data)
+    # get the data subsetted by dataset_id
+    subsets,unique_dataset_ids = get_subsets(data,dataset_id)
     
     # get the survival days and exit status (event binary indicator) for each group
     survival_days = [get_survival_days(subset) for subset in subsets]
-    exit_status = [get_exit_status(subset,mode) for subset in subsets]
+    exit_status = [get_exit_status(subset) for subset in subsets]
     
     # get the survival function and censored data data frame for each group
-    survival_functions, censored_dfs = zip(*[get_survival_function_and_censored_dfs(s,e,d.sample_id) for s,e,d in zip(survival_days,exit_status,subsets)])    
+    survival_functions, censored_dfs = zip(*[get_survival_function_and_censored_dfs(s,e,d.donor_id) for s,e,d in zip(survival_days,exit_status,subsets)])    
     
     # write the survival functions to a file
-    survival_functions_df = concatenate_dfs_add_label(survival_functions,unique_labels)
+    survival_functions_df = concatenate_dfs_add_dataset_id(survival_functions,unique_dataset_ids)
     survival_functions_df.to_csv(os.path.join(root_path,"result.tsv"), sep="\t", index=False)
     
     # write the censored data to a file
-    censored_df = concatenate_dfs_add_label(censored_dfs,unique_labels)
+    censored_df = concatenate_dfs_add_dataset_id(censored_dfs,unique_dataset_ids)
     censored_df.to_csv(os.path.join(root_path,"censored.tsv"), sep="\t", index=False)
        
     # perform the logrank test if there is more than one group
-    if len(unique_labels) > 1:
-        logrank = logrank_test(pd.concat(survival_days),pd.concat(exit_status),label)
+    if len(unique_dataset_ids) > 1:
+        logrank = logrank_test(pd.concat(survival_days),pd.concat(exit_status),dataset_id)
         logrank.to_csv(os.path.join(root_path,"logrank_test.tsv"), sep="\t", index=False)
     
 
